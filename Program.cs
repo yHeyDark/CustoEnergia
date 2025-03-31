f49b287b480bdbb282baf5c018eb0116fa58fd41
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-03-31 20:47:11 -0300

    Custo de Energia

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double conAparelho, hrUsoDia, CusEnergia, Estimado;
+
+Console.Write("Digite o Consumo em kWh/mês: ");
+conAparelho = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Horas de uso por dia: ");
+hrUsoDia = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Custo de energia em R$/kWh");
+CusEnergia = Convert.ToDouble(Console.ReadLine());
+
+Estimado = conAparelho * hrUsoDia * CusEnergia;
+
+Console.Clear();
+Console.WriteLine("--- Custo de Energia ---");
+Console.WriteLine($"Consumo do aparelho (em kWh/mês)...: {conAparelho}");
+Console.WriteLine($"Horas de uso por dia...............: {hrUsoDia}");
+Console.WriteLine($"Custo da energia (em R$/kWh).......: {CusEnergia}");
+Console.WriteLine($"Custo estimado: R${Estimado:N6}");
